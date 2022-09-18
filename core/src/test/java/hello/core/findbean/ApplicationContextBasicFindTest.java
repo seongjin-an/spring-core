8ef7d48c6e85e0f556d4a743a00376193a47898f
seongjin-an <seongjin4us@gmail.com>
2022-09-18 16:35:28 +0900

    스프링 빈 조회 - 기본

---
+++ core/src/test/java/hello/core/findbean/ApplicationContextBasicFindTest.java
@@ -0,0 +1,47 @@
+package hello.core.findbean;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApplicationContextBasicFindTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @DisplayName("빈 이름으로 조회")
+    @Test
+    public void findBeanByName() throws Exception{
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @DisplayName("이름 없이 타입으로만 조회")
+    @Test
+    public void findBeanByType() throws Exception{
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @DisplayName("구체 타입으로 조회")
+    @Test
+    public void findBeanByName2() throws Exception{
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @DisplayName("빈 이름으로 조회X")
+    @Test
+    public void findByNameX() throws Exception{
+//        MemberService xxxx = ac.getBean("xxxx", MemberService.class);
+        assertThrows(NoSuchBeanDefinitionException.class, () -> ac.getBean("xxxx", MemberService.class));
+        assertThatThrownBy(() -> ac.getBean("xxxx", MemberService.class))
+                .isInstanceOf(NoSuchBeanDefinitionException.class);
+    }
+}
