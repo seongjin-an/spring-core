5b9dd22b02e7ff7290d2b92090b1e433148270dd
anseongjin <ansj@eseict.com>
2022-09-22 15:28:11 +0900

    빈 등록 초기화, 소멸 메서드

--- core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -18,7 +18,7 @@ public class BeanLifeCycleTest {
     }
     @Configuration
     static class LifeCycleConfig {
-        @Bean
+        @Bean(initMethod = "init", destroyMethod = "close")
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             System.out.println("/////////////CHECK POINT/////////////");
--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {//implements InitializingBean, DisposableBean {
     private String url;
 
     public NetworkClient() {
@@ -16,22 +13,35 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     public void connect() {
         System.out.println("connect : " + url);
     }
+
     public void call(String message) {
         System.out.println("call : " + url + " message = " + message);
     }
+
     // 서비스 종료 시 호출
     public void disconnect() {
         System.out.println("close " + url);
     }
-    @Override
-    public void afterPropertiesSet() throws Exception {
+
+    public void init() throws Exception {
         System.out.println("NetworkClient.afterPropertiesSet");
         connect();
         call("초기화 연결 메시지");
     }
-    @Override
-    public void destroy() throws Exception {
+
+    public void close() throws Exception {
         System.out.println("NetworkClient.destroy");
         disconnect();
     }
+//    @Override
+//    public void afterPropertiesSet() throws Exception {
+//        System.out.println("NetworkClient.afterPropertiesSet");
+//        connect();
+//        call("초기화 연결 메시지");
+//    }
+//    @Override
+//    public void destroy() throws Exception {
+//        System.out.println("NetworkClient.destroy");
+//        disconnect();
+//    }
 }
