41216f115db65137363bbe6052d92fc9c1fa7924
seongjin-an <seongjin4us@gmail.com>
2022-09-23 13:06:21 +0900

    싱글톤 빈과 프로토타입 빈을 함께 사용 시 provider 로 문제 해결

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -3,12 +3,16 @@ package hello.core.scope;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 public class SingletonWithPrototypeTest1 {
     @DisplayName("FIND PROTOTYPE")
@@ -38,6 +42,51 @@ public class SingletonWithPrototypeTest1 {
         Assertions.assertThat(count2).isEqualTo(2);
     }
 
+    //기대한대로 되기는 하지만 추천하지 않는 방법
+    @DisplayName("applicationContext 빈조회(DL) && 싱글톤 클라이언트 -> 프로토타입")
+    @Test
+    void singletonClientUsePrototype2() {
+        AnnotationConfigApplicationContext ac =
+                new AnnotationConfigApplicationContext(ClientBean2.class, PrototypeBean.class);
+        ClientBean2 clientBean1 = ac.getBean(ClientBean2.class);
+        int count1 = clientBean1.logic();
+        Assertions.assertThat(count1).isEqualTo(1);
+
+        ClientBean2 clientBean2 = ac.getBean(ClientBean2.class);
+        int count2 = clientBean2.logic();
+        Assertions.assertThat(count2).isEqualTo(1);
+    }
+
+    //추천
+    @DisplayName("ObjectProvider && 싱글톤 클라이언트 -> 프로토타입")
+    @Test
+    void singletonClientUsePrototype3() {
+        AnnotationConfigApplicationContext ac =
+                new AnnotationConfigApplicationContext(ClientBean3.class, PrototypeBean.class);
+        ClientBean3 clientBean1 = ac.getBean(ClientBean3.class);
+        int count1 = clientBean1.logic();
+        Assertions.assertThat(count1).isEqualTo(1);
+
+        ClientBean3 clientBean2 = ac.getBean(ClientBean3.class);
+        int count2 = clientBean2.logic();
+        Assertions.assertThat(count2).isEqualTo(1);
+    }
+
+    //추천
+    @DisplayName("javax inject && 싱글톤 클라이언트 -> 프로토타입")
+    @Test
+    void singletonClientUsePrototype4() {
+        AnnotationConfigApplicationContext ac =
+                new AnnotationConfigApplicationContext(ClientBean4.class, PrototypeBean.class);
+        ClientBean4 clientBean1 = ac.getBean(ClientBean4.class);
+        int count1 = clientBean1.logic();
+        Assertions.assertThat(count1).isEqualTo(1);
+
+        ClientBean4 clientBean2 = ac.getBean(ClientBean4.class);
+        int count2 = clientBean2.logic();
+        Assertions.assertThat(count2).isEqualTo(1);
+    }
+
     @Scope("singleton")
     static class ClientBean {
         private final PrototypeBean prototypeBean; // 생성 시점에 주입
@@ -47,6 +96,42 @@ public class SingletonWithPrototypeTest1 {
         }
 
         public int logic() {
+            prototypeBean.addAccount();
+            return prototypeBean.getCount();
+        }
+    }
+
+    @Scope("singleton")
+    static class ClientBean2 {
+        @Autowired private ApplicationContext ac;
+
+        public int logic() {
+            PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
+            prototypeBean.addAccount();
+            return prototypeBean.getCount();
+        }
+    }
+
+    @Scope("singleton")
+    static class ClientBean3 {
+        @Autowired
+//        private ObjectFactory<PrototypeBean> prototypeBeanProvider;
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+
+        public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();//찾아준다.
+            prototypeBean.addAccount();
+            return prototypeBean.getCount();
+        }
+    }
+
+    @Scope("singleton")
+    static class ClientBean4 {
+        @Autowired
+        private Provider<PrototypeBean> prototypeBeanProvider;
+
+        public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();//찾아준다.
             prototypeBean.addAccount();
             return prototypeBean.getCount();
         }
