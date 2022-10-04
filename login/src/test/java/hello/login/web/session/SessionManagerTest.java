6350486dac459e480b2abf9a8c418aa7045028ee
seongjin-an <seongjin4us@gmail.com>
2022-10-04 23:59:04 +0900

    로그인 처리하기 - 세션 직접 만들기

---
+++ login/src/test/java/hello/login/web/session/SessionManagerTest.java
@@ -0,0 +1,38 @@
+package hello.login.web.session;
+
+import hello.login.domain.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import javax.servlet.http.HttpServletResponse;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SessionManagerTest {
+    private SessionManager sessionManager = new SessionManager();
+
+    @DisplayName("sessionTest")
+    @Test
+    public void sessionTest() throws Exception{
+        //세션 생성, 서버 -> 클라이언트
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        Member member = new Member();
+        sessionManager.createSession(member, response);
+
+        //요청에 응답 쿠키 저장, 클라이언트 -> 서버
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setCookies(response.getCookies());
+
+        //세션 조회
+        Object result = sessionManager.getSession(request);
+        Assertions.assertThat(result).isEqualTo(member);
+
+        //세션 만료
+        sessionManager.expire(request);
+        Object expired = sessionManager.getSession(request);
+        Assertions.assertThat(expired).isNull();
+    }
+}
