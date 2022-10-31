309d6f0b708c9b6c7b125a72d9693a9465059b48
seongjin-an <seongjin4us@gmail.com>
2022-10-31 22:54:47 +0900

    스프링 트랜잭션 전파5 - 외부 롤백

--- springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -101,4 +101,20 @@ public class BasicTxTest {
         log.info("외부 트랜잭션 커밋");
         txManager.commit(outer);
     }
+    
+    @DisplayName("외부 트랜잭션 롤백")
+    @Test
+    public void outer_rollback() throws Exception{
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("내부 트랜잭션 커밋");
+        txManager.commit(inner);
+
+        log.info("외부 트랜잭션 롤백");
+        txManager.rollback(outer);
+    }
+
 }
